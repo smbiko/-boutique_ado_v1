73f879d259e657ff82e7deff148aa192e3ed544e
Siyasanga Mbiko <siyasangambiko@mac.home>
2025-06-08 17:37:10 +0100

    Added mobile header and main navbar

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'django-insecure-5+i=t#@a*gfm(uu6m+g(-jsaq5ucdh5oys#-j*jzd@+^gl)+a#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = []
 
 
