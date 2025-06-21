ccf73eab4951a6f2c5c483f57a0506a693f13502
Siyasanga Mbiko <siyasangambiko@mac.home>
2025-06-21 16:03:15 +0100

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51RcS062KsWtBY6prHRjC0FXWE0xon6vAyqSqk74jXlSkiDBbRgJX4YV5FCL7D6KWOtvLi5Tea5OmOrlxMZxt44fk00nsjPjeA6',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
